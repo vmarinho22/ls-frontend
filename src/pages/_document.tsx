bec42a4e1e49d3924dbcce1fc8fa9e7a6ca3f5ad
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-09-04 18:39:36 -0300

    chore: moving project to source past and add chakra color mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
+import { ColorModeScript } from '@chakra-ui/react';
 import { Head, Html, Main, NextScript } from 'next/document';
 import { FunctionComponent } from 'react';
+import theme from '../theme';
 
 const Document: FunctionComponent = () => (
   <Html>
     <Head />
     <body>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Main />
       <NextScript />
     </body>
