da23e945724006d7009d5a07829bc811f6d32af5
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-09-04 20:46:15 -0300

    feat(theme): sending theme in chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import { FunctionComponent } from 'react';
 import '../styles/globals.css';
+import theme from '../theme';
 
-const App: FunctionComponent<any> = ({ Component, pageProps }: AppProps) => {
+const App: any = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,26 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 // 2. Add your color mode config
-const config: ThemeConfig = {
+
+const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
+  colors: {
+    'blue-sys': { 100: '#1098F7', 200: '#044c7f' },
+    'cream-sys': '#B89E97',
+    'light-pink-grey-sys': '#DECCCC',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: mode('#FFFAFB', '#0f0f0f'),
+      },
+    },
+  },
 };
 
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme(config);
 
 export default theme;
