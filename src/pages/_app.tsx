a3157601d6723e86784ca558dce9ddd60680d186
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-09-07 13:32:10 -0300

    feat(toast): adding react-toastify and set default configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 import theme from '../theme';
 
@@ -7,6 +9,7 @@ const App: any = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <ToastContainer />
     </ChakraProvider>
   );
 };
