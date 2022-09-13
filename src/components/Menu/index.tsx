6bda15e483066180bd99c120bea190779650fdff
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-09-12 22:34:04 -0300

    fix(menu): fixing responsiveness of menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -21,8 +21,8 @@ const Menu: FC<any> = (props: any) => {
       as="nav"
       width={
         isExpanded
-          ? { base: '22%', md: '22%', lg: '14%', xl: '12%' }
-          : { base: '5%', md: '5%', lg: '4%', xl: '3%' }
+          ? { base: '50%', md: '22%', lg: '14%', xl: '12%' }
+          : { base: '10%', md: '5%', lg: '4%', xl: '3%' }
       }
       transition="0.3s ease-out"
       padding="1em 3px 0px 3px"
