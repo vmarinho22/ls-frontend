b675748a6ca53b536536df98b6cca52c0ed91d99
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-09-28 22:49:30 -0300

    feat(simple-table): adding animation on table

--- src/components/Tables/SimpleTable/index.tsx
+++ src/components/Tables/SimpleTable/index.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Flex,
+  ScaleFade,
   Table,
   TableContainer,
   Tbody,
@@ -10,7 +11,7 @@ import {
   Tr,
   useColorModeValue,
 } from '@chakra-ui/react';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
 interface Heading {
@@ -33,7 +34,9 @@ const SimpleTable: FC<Props> = ({
   itensPerPage = 10,
   showIndex = false,
 }: Props) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const indexColor = useColorModeValue('#cacaca', '#414141');
   const paginationStyleButton = {
     cursor: 'pointer',
@@ -55,44 +58,52 @@ const SimpleTable: FC<Props> = ({
     }
   };
 
+  useEffect(() => {
+    setIsOpen(true);
+
+    return () => setIsOpen(false);
+  }, []);
+
   return (
     <Flex width="100%" direction="column">
-      <TableContainer>
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              {showIndex && <Th></Th>}
-              {heading.map((head: Heading) => (
-                <Th key={`heading-${title}-${head.title}`}>
-                  <Flex alignItems="center">
-                    <Box>{head?.icon}</Box>
-                    {head?.title}
-                  </Flex>
-                </Th>
-              ))}
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data
-              .slice(
-                currentIndex * itensPerPage,
-                currentIndex * itensPerPage + itensPerPage
-              )
-              ?.map((item: any, index: number) => (
-                <Tr key={`table-item-${title}-${index}`}>
-                  {showIndex && (
-                    <Td color={indexColor}>
-                      {index + currentIndex * itensPerPage + 1}
-                    </Td>
-                  )}
-                  {Object.values(item).map((current: any, index: number) => (
-                    <Td key={`current-${title}-${index}`}>{current}</Td>
-                  ))}
-                </Tr>
-              ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+      <ScaleFade initialScale={0.8} in={isOpen}>
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                {showIndex && <Th></Th>}
+                {heading.map((head: Heading) => (
+                  <Th key={`heading-${title}-${head.title}`}>
+                    <Flex alignItems="center">
+                      <Box>{head?.icon}</Box>
+                      {head?.title}
+                    </Flex>
+                  </Th>
+                ))}
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data
+                .slice(
+                  currentIndex * itensPerPage,
+                  currentIndex * itensPerPage + itensPerPage
+                )
+                ?.map((item: any, index: number) => (
+                  <Tr key={`table-item-${title}-${index}`}>
+                    {showIndex && (
+                      <Td color={indexColor}>
+                        {index + currentIndex * itensPerPage + 1}
+                      </Td>
+                    )}
+                    {Object.values(item).map((current: any, index: number) => (
+                      <Td key={`current-${title}-${index}`}>{current}</Td>
+                    ))}
+                  </Tr>
+                ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </ScaleFade>
       <Flex justify="flex-end" margin="8px 4px 0px 0px">
         <Box sx={paginationStyleButton} mr="10px">
           <HiChevronLeft size="1.8em" onClick={handleSubPage} />
