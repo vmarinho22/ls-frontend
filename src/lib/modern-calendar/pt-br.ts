fb27efda88836c347f642fb03b90e67a2193a00e
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-10-08 15:44:16 -0300

    feat(users): adding create user page

---
+++ src/lib/modern-calendar/pt-br.ts
@@ -0,0 +1,33 @@
+const ptBr = {
+  name: 'pt-br',
+  months: [
+    ['Janeiro', 'Jan'],
+    ['Fevereiro', 'Fev'],
+    ['Março', 'Mar'],
+    ['Abril', 'Abr'],
+    ['Maio', 'Mai'],
+    ['Junho', 'Jun'],
+    ['Julho', 'Jul'],
+    ['Agosto', 'Ago'],
+    ['Setembro', 'Set'],
+    ['Outubro', 'Out'],
+    ['Novembro', 'Nov'],
+    ['Dezembro', 'Dez'],
+  ],
+  weekDays: [
+    ['Sábado', 'Sab'],
+    ['Domingo', 'Dom'],
+    ['Segunda', 'Seg'],
+    ['Terça', 'Ter'],
+    ['Quarta', 'Qua'],
+    ['Quinta', 'Qui'],
+    ['Sexta', 'Sex'],
+  ],
+  digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  meridiems: [
+    ['AM', 'am'],
+    ['PM', 'pm'],
+  ],
+};
+
+export default ptBr;
